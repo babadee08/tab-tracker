ea2df147c337473c26a14e7ecf9c447cb71f2f09
Damilare <damilare.durojaye@gmail.com>
2017-09-10 06:05:40 +0100

    implemented Logout

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -15,6 +15,7 @@
     <v-toolbar-items>
       <v-btn v-if="!$store.state.isUserLoggedIn" flat dark to="login">Login</v-btn>
       <v-btn v-if="!$store.state.isUserLoggedIn" flat dark to="register">Sign Up</v-btn>
+      <v-btn v-if="$store.state.isUserLoggedIn" flat dark @click="logout">Log Out</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -24,6 +25,11 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({name: 'root'})
     }
   }
 }
